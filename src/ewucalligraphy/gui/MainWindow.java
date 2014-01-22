6377dcb581b443bc3350dd1cf0b0f65a269df190
dave <dave@localhost.localdomain>
2014-01-21 17:02:21 -0800

    Removed unused Imports.

--- src/ewucalligraphy/gui/MainWindow.java
+++ src/ewucalligraphy/gui/MainWindow.java
@@ -19,9 +19,7 @@ package ewucalligraphy.gui;
 
 import ewucalligraphy.image.WholeImage;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
