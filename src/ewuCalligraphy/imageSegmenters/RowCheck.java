bed784f01ef5faac898be9d212ec7f7254e64e55
dave <dave@localhost.localdomain>
2014-02-01 10:33:54 -0800

    Added note for SQLjet.

--- src/ewuCalligraphy/imageSegmenters/RowCheck.java
+++ src/ewuCalligraphy/imageSegmenters/RowCheck.java
@@ -9,5 +9,32 @@ package ewuCalligraphy.imageSegmenters;
 
 public class RowCheck
 {
+    public static void rowSums(int[][] image)
+    {
+       int y, sum;
+       for(int x = 0; x < image.length; ++x)
+       {
+           sum = 0;
+           for(y = 0; y < image[x].length; ++y)
+           {
+               sum += image[x][y];
+           }
+           System.out.println(sum);
+       }
+    }
     
+    public static void columnSums(int[][] image)
+    {
+        int x, sum;
+        
+        for(int y = 0; y < image[0].length; ++y)
+        {
+            sum = 0;
+            for(x = 0; x < image.length; ++x)
+            {
+                sum += image[x][y];
+            }
+            System.out.println(sum);
+        }
+    }
 }
